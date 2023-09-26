c857f48baa449a75d4e5aca774785090945f2fc2
giovannimoscati <giovannimoscati@gmail.com>
2023-09-26 23:05:31 +0200

    build fix

--- dist/logger.js
+++ dist/logger.js
@@ -4,21 +4,15 @@ Object.defineProperty(exports, "__esModule", {
   value: true
 });
 exports.default = void 0;
-
 var _humanizeNumber = _interopRequireDefault(require("humanize-number"));
-
 var _bytes = _interopRequireDefault(require("bytes"));
-
 var _chalk = _interopRequireDefault(require("chalk"));
-
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-
-function ownKeys(object, enumerableOnly) { var keys = Object.keys(object); if (Object.getOwnPropertySymbols) { var symbols = Object.getOwnPropertySymbols(object); if (enumerableOnly) { symbols = symbols.filter(function (sym) { return Object.getOwnPropertyDescriptor(object, sym).enumerable; }); } keys.push.apply(keys, symbols); } return keys; }
-
-function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i] != null ? arguments[i] : {}; if (i % 2) { ownKeys(Object(source), true).forEach(function (key) { _defineProperty(target, key, source[key]); }); } else if (Object.getOwnPropertyDescriptors) { Object.defineProperties(target, Object.getOwnPropertyDescriptors(source)); } else { ownKeys(Object(source)).forEach(function (key) { Object.defineProperty(target, key, Object.getOwnPropertyDescriptor(source, key)); }); } } return target; }
-
-function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
-
+function ownKeys(e, r) { var t = Object.keys(e); if (Object.getOwnPropertySymbols) { var o = Object.getOwnPropertySymbols(e); r && (o = o.filter(function (r) { return Object.getOwnPropertyDescriptor(e, r).enumerable; })), t.push.apply(t, o); } return t; }
+function _objectSpread(e) { for (var r = 1; r < arguments.length; r++) { var t = null != arguments[r] ? arguments[r] : {}; r % 2 ? ownKeys(Object(t), !0).forEach(function (r) { _defineProperty(e, r, t[r]); }) : Object.getOwnPropertyDescriptors ? Object.defineProperties(e, Object.getOwnPropertyDescriptors(t)) : ownKeys(Object(t)).forEach(function (r) { Object.defineProperty(e, r, Object.getOwnPropertyDescriptor(t, r)); }); } return e; }
+function _defineProperty(obj, key, value) { key = _toPropertyKey(key); if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+function _toPropertyKey(arg) { var key = _toPrimitive(arg, "string"); return typeof key === "symbol" ? key : String(key); }
+function _toPrimitive(input, hint) { if (typeof input !== "object" || input === null) return input; var prim = input[Symbol.toPrimitive]; if (prim !== undefined) { var res = prim.call(input, hint || "default"); if (typeof res !== "object") return res; throw new TypeError("@@toPrimitive must return a primitive value."); } return (hint === "string" ? String : Number)(input); }
 var _default = ({
   logger = console.log,
   addFields,
@@ -26,30 +20,27 @@ var _default = ({
 } = {}) => async (ctx, next) => {
   // check if this log instance is excluded
   const excluded = !exclude || !exclude(ctx);
-
   if (!excluded) {
     return next();
-  } // calculate the start time
+  }
 
-
+  // calculate the start time
   const start = ctx[Symbol.for('request-received.startTime')] ? ctx[Symbol.for('request-received.startTime')].getTime() : Date.now();
   const customInfo = addFields ? addFields(ctx) : {};
-
   const baseLogInfo = _objectSpread({
     name: process.env.npm_package_name,
     version: process.env.npm_package_version,
     ip: ctx.ips.length > 0 ? ctx.ips[0] : ctx.ip,
     method: ctx.method,
     url: ctx.originalUrl
-  }, customInfo); // log the request
+  }, customInfo);
 
-
+  // log the request
   logger(_chalk.default.yellow('request'));
   logger(_objectSpread({
     time: new Date().toISOString(),
     timestamp: new Date().getTime()
   }, baseLogInfo));
-
   try {
     await next();
   } catch (error) {
@@ -58,17 +49,17 @@ var _default = ({
     error.message && logger(error.message);
     logger(error);
     throw error;
-  } // log when the response is finished or closed, whichever happens first
+  }
 
-
+  // log when the response is finished or closed, whichever happens first
   const {
     res
   } = ctx;
-
   const done = () => {
     res.removeListener('finish', onfinish);
-    res.removeListener('close', onclose); // log the response
+    res.removeListener('close', onclose);
 
+    // log the response
     logger(_chalk.default.yellow('response'));
     logger(_objectSpread(_objectSpread({
       time: new Date().toISOString(),
@@ -79,32 +70,25 @@ var _default = ({
       response_time: getResponseTime(start)
     }));
   };
-
   const onfinish = done.bind(null, 'finish');
   const onclose = done.bind(null, 'close');
   res.once('finish', onfinish);
   res.once('close', onclose);
 }; // get the human readable response length
-
-
 exports.default = _default;
-
 const getResponseLength = ctx => {
   const length = ctx.response.length;
   const status = ctx.status;
-
   if ([204, 205, 304].includes(status)) {
     return '';
   }
-
   if (length === null) {
     return '-';
   }
-
   return (0, _bytes.default)(length).toLowerCase();
-}; // show the response time in human readable format.
+};
 
-
+// show the response time in human readable format.
 const getResponseTime = start => {
   const delta = Date.now() - start;
   return (0, _humanizeNumber.default)(delta < 10000 ? delta + 'ms' : Math.round(delta / 1000) + 's');
